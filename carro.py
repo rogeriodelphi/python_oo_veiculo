8504de8e1adced1f4df403bd2054642e946cc2b7
rogeriodelphi <rogeriodelphi@gmail.com>
2022-08-15 01:41:44 -0300

    Criando as classes e definindo os atributos

---
+++ carro.py
@@ -0,0 +1,7 @@
+class carro():
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):
+        self.cor = cor
+        self.qtd_portas = qtd_portas
+        self.tipo_combustivel = tipo_combustivel
+        self.potencia = potencia
+
